cfab69914e3a7ee86fd4b6274316216abafe219a
AbuSohan <sohan@inflexionpointbd.com>
2022-07-07 11:24:52 +0600

    fetch data from database and output to webpage

--- myclub/events/urls.py
+++ myclub/events/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('<int:year>/<str:month>/', views.home, name = 'home'),
+    path('events', views.all_events, name = 'all-events'),
 ]
 
 # path converters
